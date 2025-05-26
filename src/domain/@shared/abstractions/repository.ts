2b3c5718ec389e66a2bc9a3cb9761667ff791903
DevVictor19 <antoniovictor12@live.com>
2025-05-26 11:40:03 -0300

    feat: add repository abstraction

---
+++ src/domain/@shared/abstractions/repository.ts
@@ -0,0 +1,17 @@
+import { BaseEntity } from "./entity";
+
+export interface PaginatedResult<E extends BaseEntity> {
+  page: number;
+  limit: number;
+  total: number;
+  data: E[];
+}
+
+export interface BaseRepository<E extends BaseEntity> {
+  save(model: E): Promise<E>;
+  update(id: string, model: Partial<E>): Promise<E | null>;
+  delete(id: string): Promise<void>;
+  findById(id: string): Promise<E | null>;
+  findAll(): Promise<E[]>;
+  findPaginated(page: number, limit: number): Promise<PaginatedResult<E>>;
+}
