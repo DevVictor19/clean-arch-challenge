6f65027408a5ba17bec3d3dc4e5d38d7f1999097
DevVictor19 <antoniovictor12@live.com>
2025-05-26 13:01:47 -0300

    feat: add client dtos

---
+++ src/domain/clients/dtos.ts
@@ -0,0 +1,11 @@
+export interface CreateClientDTO {
+  name: string;
+  email: string;
+  phone: string;
+}
+
+export interface UpdateClientDTO {
+  name: string;
+  email: string;
+  phone: string;
+}
