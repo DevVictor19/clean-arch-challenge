26efc64b070810e2b53a451a2dc7b07d75fa95a2
DevVictor19 <antoniovictor12@live.com>
2025-05-26 15:22:24 -0300

    feat: implement client entity validate method

--- src/domain/clients/entity.ts
+++ src/domain/clients/entity.ts
@@ -1,4 +1,12 @@
-import { BaseEntity, BaseEntityProps } from "../@shared/abstractions/entity";
+// no mundo ideal era melhor isolar dependências externas do domínio (zod)
+// mas vai ficar muito complexo...
+import { z } from "zod";
+
+import {
+  BaseEntity,
+  BaseEntityProps,
+  ValidateError,
+} from "../@shared/abstractions/entity";
 
 export type ClientProps = BaseEntityProps & {
   name: string;
@@ -16,5 +24,50 @@ export class Client extends BaseEntity {
     this.name = name;
     this.email = email;
     this.phone = phone;
+  }
+
+  validate(): ValidateError {
+    const schema = z.object({
+      _id: z.string().uuid(),
+      name: z
+        .string()
+        .min(1, "Nome é obrigatório")
+        .max(55, "Nome deve ter no máximo 55 dígitos")
+        .trim(),
+      email: z.string().email("Email inválido").trim(),
+      phone: z
+        .string()
+        .min(10, "O telefone deve ter no mínimo 10 dígitos")
+        .trim()
+        .max(15, "O telefone deve ter no máximo 15 dígitos")
+        .trim(),
+      created_at: z.date(),
+      updated_at: z.date(),
+    });
+
+    const result = schema.safeParse({
+      _id: this._id,
+      name: this.name,
+      email: this.email,
+      phone: this.phone,
+      created_at: this.created_at,
+      updated_at: this.updated_at,
+    });
+
+    if (result.success) {
+      return { valid: true };
+    }
+
+    const errors: Record<string, string> = {};
+
+    for (const issue of result.error.issues) {
+      const field = issue.path[0] as string;
+      errors[field] = issue.message;
+    }
+
+    return {
+      valid: false,
+      errors,
+    };
   }
 }
