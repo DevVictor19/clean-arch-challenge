228bfb48649e80336ba033559068c28837154957
DevVictor19 <antoniovictor12@live.com>
2025-05-27 11:39:31 -0300

    fix: implement rabbitmq connection with retries

--- src/infra/rabbitmq/connection.ts
+++ src/infra/rabbitmq/connection.ts
@@ -1,28 +1,52 @@
 import amqp from "amqplib";
 import { getEnv } from "../env/env-config";
 
-let channel: amqp.Channel;
+let connection: amqp.ChannelModel | null = null;
 
-export async function connectRabbitMQ() {
-  try {
-    console.log("Attempting connection to rabbitmq...");
-    const connection = await amqp.connect(getEnv().rabbitmq.url);
-    channel = await connection.createChannel();
-    console.log("RabbitMQ connected...");
-  } catch (error) {
-    console.error("Failed to connect to rabbitmq", error);
+// podemos ter mais channels depois...
+let channel: amqp.Channel | null = null;
+
+export async function connectRabbitMQ(
+  retries: number = 5,
+  delay: number = 3000
+) {
+  for (let attempt = 1; attempt <= retries; attempt++) {
+    try {
+      console.log(`Attempt ${attempt}: Connecting to RabbitMQ...`);
+      connection = await amqp.connect(getEnv().rabbitmq.url);
+      console.log("RabbitMQ connected.");
+      return connection;
+    } catch (error) {
+      console.error(`Attempt ${attempt} failed:`, error);
+
+      if (attempt < retries) {
+        console.log(`Retrying in ${delay / 1000} seconds...`);
+        await new Promise((res) => setTimeout(res, delay));
+      } else {
+        console.error("All retries exhausted. Could not connect to RabbitMQ.");
+        throw error;
+      }
+    }
   }
 }
 
-export function getRabbitMQConnection() {
-  if (!channel) {
+export async function getRabbitMQChannel() {
+  if (!connection) {
     throw new Error("RabbitMQ connection not initialized");
+  }
+
+  if (!channel) {
+    channel = await connection.createChannel();
   }
 
   return channel;
 }
 
 export async function disconnectRabbitMQ() {
+  if (connection) {
+    await connection.close();
+  }
+
   if (channel) {
     await channel.close();
   }
--- src/infra/rabbitmq/queue.ts
+++ src/infra/rabbitmq/queue.ts
@@ -1,36 +1,39 @@
-import amqp from "amqplib";
 import { Queue } from "../../domain/@shared/abstractions/queue";
-import { getRabbitMQConnection } from "./connection";
+import { getRabbitMQChannel } from "./connection";
 
 export class RabbitMQueue implements Queue {
-  private channel: amqp.Channel;
-
-  constructor() {
-    this.channel = getRabbitMQConnection();
-  }
-
   async publish(queue: string, message: any): Promise<void> {
-    await this.channel.assertQueue(queue);
-    this.channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
-    console.log(`Message sent to queue "${queue}"`);
+    try {
+      const channel = await getRabbitMQChannel();
+      channel.assertQueue(queue);
+      channel.sendToQueue(queue, Buffer.from(JSON.stringify(message)));
+      console.log(`Message sent to queue "${queue}"`);
+    } catch (error) {
+      console.error("publish error: ", error);
+      throw error;
+    }
   }
 
   async consume(
     queue: string,
     onMessage: (msg: any) => Promise<void>
   ): Promise<void> {
-    await this.channel.assertQueue(queue);
-    this.channel.consume(queue, async (msg) => {
-      if (msg) {
-        try {
-          await onMessage(JSON.parse(msg.content.toString()));
-          this.channel.ack(msg);
-        } catch (error) {
-          // aqui podemos controlar o retry...
-          console.error("consume error: ", error);
+    try {
+      const channel = await getRabbitMQChannel();
+      channel.assertQueue(queue);
+      channel.consume(queue, async (msg) => {
+        if (msg) {
+          try {
+            await onMessage(JSON.parse(msg.content.toString()));
+            channel.ack(msg);
+          } catch (error) {
+            console.error("consume error: ", error);
+          }
         }
-      }
-    });
-    console.log(`Consuming messages from queue "${queue}"`);
+      });
+      console.log(`Consuming messages from queue "${queue}"`);
+    } catch (error) {
+      console.error("consume error: ", error);
+    }
   }
 }
